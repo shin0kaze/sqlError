8868bdbde7970835b3c881799432c93a3d01dcfa
shin0kaze <Kirill.moskva@mail.ru>
2024-05-01 14:36:12 -0600

    first commit

---
+++ sqlPTest/Program.fs
@@ -0,0 +1,21 @@
+﻿
+open FSharp.Data.Sql
+
+let [<Literal>] resolutionPath = __SOURCE_DIRECTORY__ + @"\..\files" 
+let [<Literal>] connectionString = "Data Source=" + __SOURCE_DIRECTORY__ + @"\test.db;Mode=ReadWriteCreate"
+
+printfn $"resolution: {resolutionPath}"
+printfn $"connection: {connectionString}"
+
+type sql = SqlDataProvider<
+                DatabaseVendor = Common.DatabaseProviderTypes.SQLITE,
+                ConnectionString = connectionString,        
+                SQLiteLibrary = Common.SQLiteLibrary.MicrosoftDataSqlite,
+                ResolutionPath = resolutionPath, 
+                CaseSensitivityChange = Common.CaseSensitivityChange.ORIGINAL>
+
+[<EntryPoint>]
+let main _ = 
+    let ctx = sql.GetDataContext()
+    //let m = ctx.Main Error!
+    0
